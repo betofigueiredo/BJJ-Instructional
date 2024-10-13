c5341cab0e117f232c9a5e0bde30e03a26ef14f2
Beto <beto@2sidesof1.com>
2024-10-13 15:11:07 -0300

    feat: better flow

--- main.go
+++ main.go
@@ -23,6 +23,7 @@ const (
 	DEFENSE      = "DEFENSE"
 	SUBMISSIONS  = "SUBMISSIONS"
 	PROGRESSIONS = "PROGRESSIONS"
+	POSITIONS    = "POSITIONS"
 
 	// submissions
 	KIMURA          = "KIMURA"
@@ -46,8 +47,11 @@ const (
 	ONE_LEG = "ONE_LEG"
 
 	// positions
-	GUARDA  = "GUARDA"
-	MONTADA = "MONTADA"
+	GUARDA        = "GUARDA"
+	MONTADA       = "MONTADA"
+	COSTAS        = "COSTAS"
+	CEM_QUILOS    = "CEM_QUILOS"
+	QUATRO_APOIOS = "QUATRO_APOIOS"
 )
 
 type Option struct {
@@ -56,12 +60,14 @@ type Option struct {
 }
 
 var ATACKS_TYPES = []Option{
+	{label: "Todas as opções", value: ""},
 	{label: "Submissões (Kimura, Armlock, etc)", value: SUBMISSIONS},
 	{label: "Progressões (Raspagem, Passagem, etc)", value: PROGRESSIONS},
 }
 
 var ATACKS = map[string][]Option{
 	SUBMISSIONS: {
+		{label: "Todas as opções", value: ""},
 		{label: "Kimura", value: KIMURA},
 		{label: "Americana", value: AMERICANA},
 		{label: "Omoplata", value: OMOPLATA},
@@ -72,11 +78,22 @@ var ATACKS = map[string][]Option{
 		{label: "Ataque de pé", value: ATAQUE_PE},
 	},
 	PROGRESSIONS: {
+		{label: "Todas as opções", value: ""},
 		{label: "Raspagem", value: RASPAGEM},
 		{label: "Abertura de guarda", value: ABERTURA_GUARDA},
 		{label: "Passagem de guarda", value: PASSAGEM_GUARDA},
 		{label: "Pegada das costas", value: PEGADA_COSTAS},
 		{label: "Joelho na barriga", value: JOELHO_BARRIGA},
+	},
+}
+
+var DEFENSES = map[string][]Option{
+	POSITIONS: {
+		{label: "Todas as opções", value: ""},
+		{label: "Montada", value: MONTADA},
+		{label: "Costas", value: COSTAS},
+		{label: "Cem quilos", value: CEM_QUILOS},
+		{label: "Quatro apoios", value: QUATRO_APOIOS},
 	},
 }
 
@@ -133,6 +150,18 @@ var (
 		Background(lipgloss.Color("#7D56F4")).
 		PaddingRight(1).
 		PaddingLeft(1)
+	atackTitle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#111111")).
+			Background(lipgloss.Color("#C2DBE9")).
+			PaddingRight(1).
+			PaddingLeft(1)
+	defenseTitle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color("#111111")).
+			Background(lipgloss.Color("#ec96a7")).
+			PaddingRight(1).
+			PaddingLeft(1)
 	subtitle      = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
 	subtleStyle   = lipgloss.NewStyle().Foreground(lipgloss.Color("241"))
 	checkboxStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("212"))
@@ -141,12 +170,11 @@ var (
 )
 
 var VIEWS = map[string]string{
-	LOADING:               LOADING,
-	RESULT:                RESULT,
-	CHOOSE_ATACK_TYPE:     CHOOSE_ATACK_TYPE,
-	CHOOSE_ATACK:          CHOOSE_ATACK,
-	CHOOSE_ATACK_POSITION: CHOOSE_ATACK_POSITION,
-	CHOOSE_DEFENSE:        CHOOSE_DEFENSE,
+	CHOOSE_ATACK_TYPE: CHOOSE_ATACK_TYPE,
+	CHOOSE_ATACK:      CHOOSE_ATACK,
+	CHOOSE_DEFENSE:    CHOOSE_DEFENSE,
+	LOADING:           LOADING,
+	RESULT:            RESULT,
 }
 
 type model struct {
@@ -155,16 +183,11 @@ type model struct {
 	DefensePick Content
 	AtackType   int
 	Atack       int
+	Defense     int
 }
 
 func main() {
-	initialModel := model{
-		CHOOSE_ATACK_TYPE,
-		Content{},
-		Content{},
-		-1,
-		-1,
-	}
+	initialModel := model{CHOOSE_ATACK_TYPE, Content{}, Content{}, 0, 0, 0}
 	p := tea.NewProgram(initialModel)
 	if _, err := p.Run(); err != nil {
 		fmt.Println("could not start program:", err)
@@ -175,7 +198,7 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
-func forceUpdate(msg tea.Msg, m model) tea.Cmd {
+func forceUpdate(msg tea.Msg) tea.Cmd {
 	return func() tea.Msg {
 		time.Sleep(800 * time.Millisecond)
 		return msg
@@ -192,13 +215,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	}
 
 	switch m.CurrentView {
+	case VIEWS[CHOOSE_ATACK_TYPE]:
+		return updateChooseAtackType(msg, m)
+	case VIEWS[CHOOSE_ATACK]:
+		return updateChooseAtack(msg, m)
+	case VIEWS[CHOOSE_DEFENSE]:
+		return updateChooseDefense(msg, m)
 	case VIEWS[LOADING]:
 		m.CurrentView = VIEWS[RESULT]
 		return m, nil
-	case VIEWS[CHOOSE_ATACK_TYPE]:
-		return updateChoseAtackType(msg, m)
-	case VIEWS[CHOOSE_ATACK]:
-		return updateChoseAtack(msg, m)
 	default:
 		return m, nil
 	}
@@ -209,12 +234,14 @@ func (m model) View() string {
 	var s string
 
 	switch m.CurrentView {
+	case VIEWS[CHOOSE_ATACK_TYPE]:
+		s = chooseAtackTypeView(m)
+	case VIEWS[CHOOSE_ATACK]:
+		s = chooseAtackView(m)
+	case VIEWS[CHOOSE_DEFENSE]:
+		s = chooseDefenseView(m)
 	case VIEWS[LOADING]:
 		s = "Encontrando resultados..."
-	case VIEWS[CHOOSE_ATACK_TYPE]:
-		s = choseAtackTypeView(m)
-	case VIEWS[CHOOSE_ATACK]:
-		s = choseAtackView(m)
 	default:
 		s = resultView(m)
 	}
@@ -255,7 +282,7 @@ func findResults(m model) model {
 
 // ANCHOR: Sub-update functions
 
-func updateChoseAtackType(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
+func updateChooseAtackType(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		max := len(ATACKS_TYPES) - 1
@@ -271,15 +298,18 @@ func updateChoseAtackType(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 				m.AtackType = 0
 			}
 		case "enter":
-			m.CurrentView = VIEWS[CHOOSE_ATACK]
+			if m.AtackType == 0 {
+				m.CurrentView = VIEWS[CHOOSE_DEFENSE]
+			} else {
+				m.CurrentView = VIEWS[CHOOSE_ATACK]
+			}
 			return m, nil
 		}
 	}
-
 	return m, nil
 }
 
-func updateChoseAtack(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
+func updateChooseAtack(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		options := ATACKS[ATACKS_TYPES[m.AtackType].value]
@@ -296,12 +326,36 @@ func updateChoseAtack(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
 				m.Atack = 0
 			}
 		case "enter":
-			m = findResults(m)
-			m.CurrentView = VIEWS[LOADING]
-			return m, forceUpdate(msg, m)
+			m.CurrentView = VIEWS[CHOOSE_DEFENSE]
+			return m, nil
 		}
 	}
 
+	return m, nil
+}
+
+func updateChooseDefense(msg tea.Msg, m model) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		options := DEFENSES[POSITIONS]
+		max := len(options) - 1
+		switch msg.String() {
+		case "j", "down":
+			m.Defense++
+			if m.Defense > max {
+				m.Defense = max
+			}
+		case "k", "up":
+			m.Defense--
+			if m.Defense < 0 {
+				m.Defense = 0
+			}
+		case "enter":
+			m = findResults(m)
+			m.CurrentView = VIEWS[LOADING]
+			return m, forceUpdate(msg)
+		}
+	}
 	return m, nil
 }
 
@@ -324,64 +378,47 @@ Vídeo: %v
 	filledString := fmt.Sprintf(
 		content,
 		title.Render("Resultado:"),
-		subtitle.Render("# Ataque"),
+		atackTitle.Render("# Ataque"),
 		m.AtackPick.name,
 		m.AtackPick.url,
-		subtitle.Render("# Defesa"),
+		defenseTitle.Render("# Defesa"),
 		m.DefensePick.name,
 		m.DefensePick.url,
 	)
-
 	return fmt.Sprint(filledString)
 }
 
-func choseAtackTypeView(m model) string {
-	c := m.AtackType
-
-	tpl := "Qual TIPO de ataque você busca?\n\n"
-	tpl += "%s\n\n"
-	tpl += subtleStyle.Render("j/k, up/down: select") + dotStyle +
-		subtleStyle.Render("enter: choose") + dotStyle +
-		subtleStyle.Render("q, esc: quit")
-
-	replacements := ""
-	questions := []interface{}{}
-	idx := 0
-
-	for _, option := range ATACKS_TYPES {
-		replacements += "%v\n"
-		questions = append(questions, checkbox(option.label, c == idx))
-		idx++
-	}
-
-	choices := fmt.Sprintf(replacements, questions...)
-
-	return fmt.Sprintf(tpl, choices)
+func chooseAtackTypeView(m model) string {
+	options := ATACKS_TYPES
+	return buildChoices(m.AtackType, atackTitle.Render("ATAQUE"), "Qual tipo de ataque você busca?", options)
 }
 
-func choseAtackView(m model) string {
-	c := m.Atack
+func chooseAtackView(m model) string {
+	options := ATACKS[ATACKS_TYPES[m.AtackType].value]
+	return buildChoices(m.Atack, atackTitle.Render("ATAQUE"), "Qual ATAQUE você busca?", options)
+}
 
-	tpl := "Qual ATAQUE você busca?\n\n"
-	tpl += "%s\n\n"
+func chooseDefenseView(m model) string {
+	options := DEFENSES[POSITIONS]
+	return buildChoices(m.Defense, defenseTitle.Render("DEFESA"), "Defesa a partir de qual posição?", options)
+}
+
+func buildChoices(field int, title string, subtitle string, options []Option) string {
+	tpl := title + "\n\n" + subtitle + "\n\n%s\n\n"
 	tpl += subtleStyle.Render("j/k, up/down: select") + dotStyle +
 		subtleStyle.Render("enter: choose") + dotStyle +
 		subtleStyle.Render("q, esc: quit")
-
+	idx := 0
 	replacements := ""
 	questions := []interface{}{}
-	idx := 0
-
-	options := ATACKS[ATACKS_TYPES[m.AtackType].value]
 
 	for _, option := range options {
 		replacements += "%v\n"
-		questions = append(questions, checkbox(option.label, c == idx))
+		questions = append(questions, checkbox(option.label, field == idx))
 		idx++
 	}
 
 	choices := fmt.Sprintf(replacements, questions...)
-
 	return fmt.Sprintf(tpl, choices)
 }
 
